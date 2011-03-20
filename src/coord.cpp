3a590f601eb6bef7a23b683211c03e4d76b1a091
Ilya Popov <ilia.b.popov@gmail.com>
2011-03-20 13:15:40 +0100

    Added copyright and licence information.

--- src/coord.cpp
+++ src/coord.cpp
@@ -1,3 +1,22 @@
+/*
+    Copyright 2011, Ilya Popov <ilia_popov@rambler.ru>
+
+    This file is part of osmview.
+
+    osmview is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    osmview is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with osmview.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "coord.hpp"
 
 #include <cmath>
