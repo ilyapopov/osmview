4d24fd686fabab26abd284288339aeb33be60722
Ilya Popov <ilia.b.popov@gmail.com>
2021-09-03 23:55:51 +0200

    Remove Boost dependency; simplify CMakeLists

--- src/filesystem.hpp
+++ src/filesystem.hpp
@@ -1,25 +1,11 @@
 #ifndef FILESYSTEM_HPP
 #define FILESYSTEM_HPP
 
-#ifdef USE_STD_FILESYSTEM
-
 #include <filesystem>
 
 namespace osmview {
+
 namespace fs = std::filesystem;
-}
-
-#else
-
-#include <boost/filesystem.hpp>
-
-namespace osmview {
-namespace fs = boost::filesystem;
-}
-
-#endif
-
-namespace osmview {
 
 fs::path get_user_cache_dir();
 
